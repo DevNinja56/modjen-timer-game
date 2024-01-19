367f8a0d7a70de3c7d3291131b14e9ec80987bef
Arslan Arjumand <arslanarjumand012@gmail.com>
2024-01-19 17:15:23 +0500

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let startTime = 0;
+  let elapsedTime = 0;
+  let timerRunning = false;
+  let speed = 1.005;
+  let timerInterval;
+
+  const timerElement = document.getElementById("timer");
+  const resetBtn = document.getElementById("resetBtn");
+
+  const handleKeyPress = (event) => {
+    if (event.code === "Space") {
+      event.preventDefault();
+      toggleTimer();
+    }
+  };
+
+  const toggleTimer = () => {
+    timerRunning = !timerRunning;
+    if (timerRunning) {
+      startTime = Date.now() - elapsedTime;
+      updateTimer();
+    } else {
+      clearInterval(timerInterval);
+    }
+  };
+
+  const resetTimer = () => {
+    timerRunning = false;
+    updateTimer();
+    startTime = Date.now();
+    elapsedTime = 0;
+  };
+
+  const updateTimer = () => {
+    clearInterval(timerInterval);
+    timerInterval = setInterval(() => {
+      if (timerRunning) {
+        const currentTime = Date.now();
+        elapsedTime = (currentTime - startTime) * speed;
+        const totalMilliseconds = Math.floor(elapsedTime);
+        const seconds = Math.floor((totalMilliseconds / 1000) % 60);
+        const milliseconds = totalMilliseconds % 1000;
+        timerElement.textContent = `${String(seconds).padStart(
+          3,
+          "0"
+        )}:${String(milliseconds).padStart(3, "0")}`;
+      } else {
+        timerElement.textContent = `${String(0).padStart(3, "0")}:${String(
+          0
+        ).padStart(3, "0")}`;
+      }
+    }, 100);
+  };
+
+  resetBtn.addEventListener("click", resetTimer);
+  document.addEventListener("keydown", handleKeyPress);
+});
